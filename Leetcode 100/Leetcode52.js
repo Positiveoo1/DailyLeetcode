0dddfacb0f33587ae8166d327b1236d541704113
Positiveoo1 <github0512@gmail.com>
2024-10-08 14:14:00 +0200

    easy

---
+++ Leetcode 100/Leetcode52.js
@@ -0,0 +1,15 @@
+//3174. Clear Digits
+var clearDigits = function(s) {  
+    let stack = [];
+    
+    for(let char of s) {
+        if(/\d/.test(char) ) {
+             if (stack.length > 0) {
+                stack.pop();  
+            }
+        }else {
+            stack.push(char)
+        }
+    }
+    return stack.join('');
+}
