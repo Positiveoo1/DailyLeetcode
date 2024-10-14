a64b30713fbed3f5fb06f6ce08f2ec8aea71dd67
Positiveoo1 <github0512@gmail.com>
2024-10-14 07:53:49 +0200

    EASY

---
+++ Leetcode 100/Leetcode86.js
@@ -0,0 +1,7 @@
+//100. Same Tree EASY
+var isSameTree = function(p, q) {
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+    if(p.val !== q.val) return false;
+    return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+};
